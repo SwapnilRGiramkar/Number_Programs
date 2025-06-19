afe9c612ad82b43a5670d5e25e737bd3c2be7255
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-06-19 20:04:39 +0530

    This Program Count User enter Number

--- CountNumber.java
+++ CountNumber.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 class CountNumber
 {
 	public static void main(String[]args)
 	{
-		int num=1235;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Eneter the Number : ");
+		int num = sc.nextInt();
 		int count=0;
 		count=(int)Math.log10(num)+1;
 		System.out.println("Count of Number is : "+count);
