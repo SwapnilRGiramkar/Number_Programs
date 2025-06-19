9a1b8fe51367da1f74640150843e7f49a162238b
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-06-19 19:56:52 +0530

    This Program Count the Number

---
+++ CountNumber.java
@@ -0,0 +1,10 @@
+class CountNumber
+{
+	public static void main(String[]args)
+	{
+		int num=1235;
+		int count=0;
+		count=(int)Math.log10(num)+1;
+		System.out.println("Count of Number is : "+count);
+	}
+}
